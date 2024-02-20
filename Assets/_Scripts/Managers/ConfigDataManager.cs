f21cfa515bf213d6baa43510296e84d977ae64a8
Slacker <99643664+Slacker2519@users.noreply.github.com>
2024-02-20 22:28:35 +0700

    add load game data and load config data

--- Assets/_Scripts/Managers/ConfigDataManager.cs
+++ Assets/_Scripts/Managers/ConfigDataManager.cs
@@ -1,10 +1,13 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ConfigDataManager : SingletonMono<ConfigDataManager>
 {
     // Config data is the base data when you start game fresh new
-    public DataAssets DataAssets => dataAssets;
-    [SerializeField] private DataAssets dataAssets;
+    public DataAssets DataAssets => _dataAssets;
+    [SerializeField] private DataAssets _dataAssets;
+
+    public void LoadGameConfigData()
+    {
+        // Assign config data from _dataAssets to config data list from class ConfigDataList
+    }
 }
