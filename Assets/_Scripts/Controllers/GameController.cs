3f82cb0837180c6c18cb4121b8bcf965d7a68d6e
Slacker <99643664+Slacker2519@users.noreply.github.com>
2024-02-27 22:33:21 +0700

    + spawn 1 enemy random on map chase player
    *removed rotate player in base character
    
    +BaseEnemy: +ChasePlayer() +TakeDamage(ame) +Oncoll
    
    *Enemy1 : BaseEnemy
    
    +GameController: +enemyList +SpawnPlayer() +SpawnEnemy +GetEnemySpawnedPos()
    
    *GameData: +CharacterStat(EnemyEnum)

--- Assets/_Scripts/Controllers/GameController.cs
+++ Assets/_Scripts/Controllers/GameController.cs
@@ -1,4 +1,3 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,15 +10,57 @@ public class GameController : MonoBehaviour
     public BaseCharacter Player => _player;
     private BaseCharacter _player;
 
+    public List<BaseEnemy> EnemiesList => _enemiesList;
+    private List<BaseEnemy> _enemiesList;
+
     private void Start()
     {
-        var character = PoolManager.Instance.SpawnCharacter(CharacterEnum.Character1);
+        _enemiesList = new List<BaseEnemy>();
+
+        SpawnPlayer();
+        SpawnEnemy();
+    }
+
+    private void SpawnPlayer()
+    {
+        var characterPicked = GameManager.Instance.Data.CharacterPicked.Name;
+        var character = PoolManager.Instance.SpawnCharacter(characterPicked);
         if (character.TryGetComponent<BaseCharacter>(out BaseCharacter player))
         {
             _player = player;
             var configManager = ConfigDataManager.Instance;
-            CharacterConfigData stat = configManager.DataAssets.GetCharacterConfig(CharacterEnum.Character1);
+            CharacterConfigData stat = configManager.DataAssets.GetCharacterConfig(characterPicked);
             _player.InitCharacterStats(stat.Name, stat.Health, stat.Defense, stat.Damage, stat.Speed);
+            //GameManager.Instance.SaveGameData();
         }
+    }
+
+    private void SpawnEnemy()
+    {
+        EnemyConfigData firstEnemy = ConfigDataManager.Instance.DataAssets.GetEnemyConfig(EnemyEnum.Enemy1);
+        GameObject enemyObj = PoolManager.Instance.SpawnEnemy(firstEnemy.Name);
+        enemyObj.transform.position = GetEnemySpawnedPos();
+        if (enemyObj.TryGetComponent<BaseEnemy>(out BaseEnemy enemy))
+        {
+            _enemiesList.Add(enemy);
+            EnemyConfigData stat = firstEnemy;
+            enemy.InitCharacterStats(stat.Name, stat.Health, stat.Defense, stat.Damage, stat.Speed);
+            //GameManager.Instance.SaveGameData();
+        }
+    }
+
+    private Vector3 GetEnemySpawnedPos()
+    {
+        float xCoordinate;
+        float yCoordinate;
+
+        int leftOrRight = Mathf.RoundToInt(Random.Range(0, 2));
+        xCoordinate = leftOrRight < 1 ? Random.Range(-.5f, -.1f) : Random.Range(1.1f, 1.5f);
+
+        int topOrBottom = Mathf.RoundToInt(Random.Range(0, 2));
+        yCoordinate = topOrBottom < 1 ? Random.Range(1.1f, 1.5f) : Random.Range(-.5f, -.1f);
+        
+        Vector3 newPos = Camera.main.ViewportToWorldPoint(new Vector3(xCoordinate, yCoordinate, 0));
+        return new Vector3(newPos.x, newPos.y, 0);
     }
 }
