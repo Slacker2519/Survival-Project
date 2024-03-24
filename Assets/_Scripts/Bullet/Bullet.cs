fcf8c18b9681422ccd81cdc180ed285499b04482
nguyen anh duc <supernaduc@gmail.com>
2024-03-24 21:50:44 +0700

    Update Bullet.cs

--- Assets/_Scripts/Bullet/Bullet.cs
+++ Assets/_Scripts/Bullet/Bullet.cs
@@ -7,8 +7,20 @@ public class Bullet : MonoBehaviour
     [SerializeField] DamageValue damageValue;
 
     public DamageValue DamageValue { get => damageValue; set => damageValue = value; }
-
+    bool isInit;
     public void Init(DamageType damageType, int _damage = 0, bool _damageAble = true) { DamageValue = new DamageValue(damageType, _damageAble, _damage); }
+    public void Setup(DamageType damageType, int _damage = 0, bool _damageAble = true)
+    {
+        if (!isInit)
+        {
+            Init(damageType, _damage, _damageAble);
+            return;
+        }
+        DamageValue.damageAble = _damageAble;
+        DamageValue.damageType = damageType;
+        DamageValue.value = _damage;
+
+    }
 
     public virtual void SelfDestruct() { }
 }
